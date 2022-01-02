ad62f4032af88d53584abe6e2267496d5cb19184
Emmanuel Gyan-Amoah <82492796+shakerbillo@users.noreply.github.com>
2022-01-02 23:26:20 +0100

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,58 @@
+package com.shaker;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+
+public String getFirstName(){
+    return this.firstName;
+}
+
+public void setFirstName(String firstName){
+     this.firstName = firstName;
+}
+
+public String getLastName(){
+    return this.lastName;
+}
+
+public void setLastName(String lastName){
+     this.lastName = lastName;
+}
+
+public int getAge(){
+    return this.age;
+}
+
+public void setAge(int age){
+    if(age < 0 || age >100) {
+        age = 0;
+    }
+     this.age = age;
+
+}
+public boolean isTeen(){
+    if(age > 12 && age < 20){
+        return true;
+    }
+        return false;
+    }
+    public String getFullName(){
+      if(firstName.isEmpty()){
+          return lastName;
+      }
+          if(lastName.isEmpty()){
+          return firstName;
+
+        }
+       if(firstName.isEmpty() && lastName.isEmpty()){
+          return " ";
+      }
+
+    return firstName + " " + lastName;
+}
+}
+
+
